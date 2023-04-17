2c0a190f1f70ad1220d1c5bdb16bbdc1bf9aad4b
efe <121581223+efetekin5@users.noreply.github.com>
2023-04-17 22:54:24 +0300

    forgot to commit but finished etch a sketch

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-for (let i=0; i<256; i++) {
-    const container = document.querySelector('.container');
-    const div = document.createElement('div');
-    div.classList.add('divs');
-    container.append(div);
+createBoard(16);
+
+function createBoard(size) {
+    let container = document.querySelector('.container');
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i=0; i<size*size; i++) {
+        let div = document.createElement('div');
+        div.style.cssText = 'border: 1px solid black';
+        div.classList.add('divs');
+        div.addEventListener('mouseover', () => {
+            div.style.cssText = 'background: black';
+        })
+        container.append(div);
+    }
 }
 
-let divs = document.querySelectorAll('.divs');
-console.log(divs);
 
-divs.addEventListener('hover', () => {
-    console.log(divs);
-    divs.style.backgroundColor = 'green';
-})
-
+function getSize() {
+    let input = prompt('Enter grid size');
+    if (input < 0 || input > 100) {
+       input = prompt('Enter a number between 1 and 100');
+    }
+    let divs = document.querySelectorAll('.divs');
+    for (let x=0; x < divs.length; x++) {
+        divs[x].style.cssText = 'border: 1px solid black; background: white;';
+    }
+    createBoard(input);
+}
